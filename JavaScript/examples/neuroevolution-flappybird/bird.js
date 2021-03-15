ac4e38b961d5054ed3ae1527a38130a7de04c4f9
iamsarthakk <sarthakkumar160627@gmail.com>
2021-03-16 00:11:54 +0530

    Neuroevolutionary Flappy Bird Updated with Tensorflow library

--- JavaScript/examples/neuroevolution-flappybird/bird.js
+++ JavaScript/examples/neuroevolution-flappybird/bird.js
@@ -1,108 +1,73 @@
-
-// Mutation function to be passed into bird.brain
-function mutate(x) {
-  if (random(1) < 0.1) {
-    let offset = randomGaussian() * 0.5;
-    let newx = x + offset;
-    return newx;
-  } else {
-    return x;
-  }
-}
-
 class Bird {
   constructor(brain) {
-    // position and size of bird
-    this.x = 64;
     this.y = height / 2;
-    this.r = 12;
+    this.x = 64;
 
-    // Gravity, lift and velocity
     this.gravity = 0.8;
     this.lift = -12;
     this.velocity = 0;
 
-    // Is this a copy of another Bird or a new one?
-    // The Neural Network is the bird's "brain"
-    if (brain instanceof NeuralNetwork) {
+    this.score = 0;
+    this.fitness = 0;
+    if (brain) {
       this.brain = brain.copy();
-      this.brain.mutate(mutate);
     } else {
       this.brain = new NeuralNetwork(5, 8, 2);
     }
-
-    // Score is how many frames it's been alive
-    this.score = 0;
-    // Fitness is normalized version of score
-    this.fitness = 0;
   }
 
-  // Create a copy of this bird
-  copy() {
-    return new Bird(this.brain);
+  dispose() {
+    this.brain.dispose();
   }
 
-  // Display the bird
   show() {
-    fill(255, 100);
     stroke(255);
-    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    fill(255, 100);
+    ellipse(this.x, this.y, 32, 32);
   }
 
-  // This is the key function now that decides
-  // if it should jump or not jump!
-  think(pipes) {
-    // First find the closest pipe
-    let closest = null;
-    let record = Infinity;
-    for (let i = 0; i < pipes.length; i++) {
-      let diff = pipes[i].x - this.x;
-      if (diff > 0 && diff < record) {
-        record = diff;
-        closest = pipes[i];
-      }
-    }
-
-    if (closest != null) {
-      // Now create the inputs to the neural network
-      let inputs = [];
-      // x position of closest pipe
-      inputs[0] = map(closest.x, this.x, width, 0, 1);
-      // top of closest pipe opening
-      inputs[1] = map(closest.top, 0, height, 0, 1);
-      // bottom of closest pipe opening
-      inputs[2] = map(closest.bottom, 0, height, 0, 1);
-      // bird's y position
-      inputs[3] = map(this.y, 0, height, 0, 1);
-      // bird's y velocity
-      inputs[4] = map(this.velocity, -5, 5, 0, 1);
-
-      // Get the outputs from the network
-      let action = this.brain.predict(inputs);
-      // Decide to jump or not!
-      if (action[1] > action[0]) {
-        this.up();
-      }
-    }
-  }
-
-  // Jump up
   up() {
     this.velocity += this.lift;
   }
 
-  bottomTop() {
-    // Bird dies when hits bottom?
-    return (this.y > height || this.y < 0);
+  mutate() {
+    this.brain.mutate(0.1);
   }
 
-  // Update bird's position based on velocity, gravity, etc.
-  update() {
-    this.velocity += this.gravity;
-    // this.velocity *= 0.9;
-    this.y += this.velocity;
+  think(pipes) {
+    // Find the closest pipe
+    let closest = null;
+    let closestD = Infinity;
+    for (let i = 0; i < pipes.length; i++) {
+      let d = pipes[i].x + pipes[i].w - this.x;
+      if (d < closestD && d > 0) {
+        closest = pipes[i];
+        closestD = d;
+      }
+    }
 
-    // Every frame it is alive increases the score
+    let inputs = [];
+    inputs[0] = this.y / height;
+    inputs[1] = closest.top / height;
+    inputs[2] = closest.bottom / height;
+    inputs[3] = closest.x / width;
+    inputs[4] = this.velocity / 10;
+    let output = this.brain.predict(inputs);
+    //if (output[0] > output[1] && this.velocity >= 0) {
+    if (output[0] > output[1]) {
+      this.up();
+    }
+  }
+
+  offScreen() {
+    return this.y > height || this.y < 0;
+  }
+
+  update() {
     this.score++;
+
+    this.velocity += this.gravity;
+    //this.velocity *= 0.9;
+    this.y += this.velocity;
   }
 }
--- JavaScript/examples/neuroevolution-flappybird/pipe.js
+++ JavaScript/examples/neuroevolution-flappybird/pipe.js
@@ -1,26 +1,15 @@
-&
 class Pipe {
   constructor() {
-
-    // How big is the empty space
-    let spacing = 125;
-    // Where is th center of the empty space
-    let centery = random(spacing, height - spacing);
-
-    // Top and bottom of pipe
-    this.top = centery - spacing / 2;
-    this.bottom = height - (centery + spacing / 2);
-    // Starts at the edge
+    this.spacing = 125;
+    this.top = random(height / 6, (3 / 4) * height);
+    this.bottom = height - (this.top + this.spacing);
     this.x = width;
-    // Width of pipe
     this.w = 80;
-    // How fast
     this.speed = 6;
   }
 
-  // Did this pipe hit a bird?
   hits(bird) {
-    if ((bird.y - bird.r) < this.top || (bird.y + bird.r) > (height - this.bottom)) {
+    if (bird.y < this.top || bird.y > height - this.bottom) {
       if (bird.x > this.x && bird.x < this.x + this.w) {
         return true;
       }
@@ -28,20 +17,17 @@ class Pipe {
     return false;
   }
 
-  // Draw the pipe
   show() {
-    stroke(255);
-    fill(200);
+    fill(255);
+    rectMode(CORNER);
     rect(this.x, 0, this.w, this.top);
     rect(this.x, height - this.bottom, this.w, this.bottom);
   }
 
-  // Update the pipe
   update() {
     this.x -= this.speed;
   }
 
-  // Has it moved offscreen?
   offscreen() {
     if (this.x < -this.w) {
       return true;
